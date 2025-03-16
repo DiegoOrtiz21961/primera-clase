007d0c244b7e3d35824916b60ada6c76fb0e428b
DiegoOrtiz21961 <parraortizdiego29@gmail.com>
2025-03-16 16:46:52 -0500

    Variable

---
+++ Variables.js
@@ -0,0 +1,32 @@
+// let miedad = 20;
+// console.log(miedad);
+// let FIFA18 = 48.5;
+// console.log("el valor del juego es ",FIFA18);
+// let letra = "z";
+// console.log("la letra almacenada es",letra);
+// let boleano = true;
+// console.log(boleano);
+
+
+// let numerofavorito = 27;
+// let name = "Diego";
+// let estudio = true
+// console.log("mi numero favorito es",numerofavorito);
+// console.log("mi nombre es",name);
+// console.log("soy estudiante",estudio);
+
+
+// let nombre = prompt("cual es su nombre?");
+// let edad = parseInt(prompt("cual es su edad?"));
+// let salario = parseFloat(prompt("cual es su salario?"));
+// console.log("su nombre es",nombre);
+// console.log("su edad es ",edad);
+// console.log(" su salario es ",salario);
+
+
+// let sexo = prompt("cual es su orientacion sexual?");
+// let carro = prompt("tiene carro? (si/no) ");
+// let direccion = prompt("cual es su direccion?");
+// console.log("su orientacion sexual es",sexo);
+// console.log("tiene carro",carro);
+// console.log("vive en",direccion)
